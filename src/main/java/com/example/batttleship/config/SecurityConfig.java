d3b0ad00ec6f237ca3f8f62b2792b578220736c6
dimitar1997 <75738859+dimitar1997@users.noreply.github.com>
2021-11-24 15:11:39 +0200

    Working on security login

--- src/main/java/com/example/batttleship/config/SecurityConfig.java
+++ src/main/java/com/example/batttleship/config/SecurityConfig.java
@@ -1,19 +1,24 @@
 package com.example.batttleship.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-private final UserDetailsService userDetailsService;
-private final PasswordEncoder passwordEncoder;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
     public SecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
         this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
@@ -24,7 +29,7 @@ private final PasswordEncoder passwordEncoder;
         http.authorizeRequests()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .antMatchers("/ships/add", "/users/logout").authenticated()
-                .antMatchers("/","users/login", "/users/register").permitAll()
+                .antMatchers("/", "users/login", "/users/register").permitAll()
                 .and()
                 .formLogin()
                 .loginPage("/users/login")
@@ -39,6 +44,7 @@ private final PasswordEncoder passwordEncoder;
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID");
     }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.
---
+++ src/main/java/com/example/batttleship/services/impl/UserDetailsIpm.java
@@ -0,0 +1,18 @@
+package com.example.batttleship.services.impl;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+
+import java.util.Collection;
+
+public class UserDetailsIpm extends User {
+    public UserDetailsIpm(String username, String password,
+                          Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+    }
+
+    public UserDetailsIpm(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+    }
+}
